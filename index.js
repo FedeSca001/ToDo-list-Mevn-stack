3404540014656a13284057789fbb0e33f8b9a36d
FedeSca001 <familadore@gmail.com>
2022-07-22 13:25:31 +0200

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5050;
@@ -8,7 +10,7 @@ app.use(express.json());
 app.use(morgan("dev")), app.use(cors());
 
 //INIT
-app.listen(port, async ()=> {
+app.listen(port, console.log('logued')/*async ()=> {
     await conectDB();
     console.log('logued',port);
-});
+}*/);
